512fcb7729c9b6522af3c5f3edc46c27350a7d5d
wanghongyu <wanghongyu@huojintech.com>
2021-08-20 18:17:32 +0800

    提交事件新增001

---
+++ springgateway/src/main/java/com/example/springgateway/controller/CommonController.java
@@ -0,0 +1,20 @@
+package com.example.springgateway.controller;
+
+import com.example.springpublic.entity.Event;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommonController {
+
+
+   /* @ResponseBody
+    @RequestMapping("/addEvent")
+    public String addOrderDetil(@RequestBody Event event){
+        String addOrderDetil = eventService.addEvent(event);
+        return addOrderDetil;
+    }*/
+
+}
