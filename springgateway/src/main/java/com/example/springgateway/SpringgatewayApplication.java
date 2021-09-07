f5a35b8501257e10d3740bc558138acd0c166fd9
wanghongyu <wanghongyu@huojintech.com>
2021-09-07 17:00:46 +0800

    test002

--- springgateway/src/main/java/com/example/springgateway/SpringgatewayApplication.java
+++ springgateway/src/main/java/com/example/springgateway/SpringgatewayApplication.java
@@ -12,11 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class SpringgatewayApplication {
 
-
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(SpringgatewayApplication.class, args);
     }
