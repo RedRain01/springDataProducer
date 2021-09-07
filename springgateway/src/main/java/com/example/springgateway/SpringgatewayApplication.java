85c0092bd186859798f91d5a461a78839e29c96d
wanghongyu <wanghongyu@huojintech.com>
2021-09-07 19:11:19 +0800

    Revert "整合登录"
    This reverts commit 6f14080038d8d83c1ff6c7bebdbbb6efed519847.

--- springgateway/src/main/java/com/example/springgateway/SpringgatewayApplication.java
+++ springgateway/src/main/java/com/example/springgateway/SpringgatewayApplication.java
@@ -1,21 +1,21 @@
 package com.example.springgateway;
 
-import com.example.springgateway.controller.CommonController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.codec.ServerCodecConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
 public class SpringgatewayApplication {
+
+
+
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringgatewayApplication.class, args);
--- springgateway/src/main/java/com/example/springgateway/controller/CommonController.java
+++ springgateway/src/main/java/com/example/springgateway/controller/CommonController.java
@@ -9,12 +9,13 @@ import com.example.springpublic.entity.event.EventUser;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +24,6 @@ import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
 
 @Slf4j
 @RestController
-@SessionAttributes("param")
 public class CommonController {
     @Autowired
     EventUserService eventUserService;
@@ -43,14 +43,7 @@ public class CommonController {
      * @return
      */
     @RequestMapping("/login")
-    public BaseOut login(@RequestBody Map<String, Object> param,ServerWebExchange exchange,WebFilterChain chain) {
-        HandshakeWebSocketService handshakeWebSocketService=new HandshakeWebSocketService();
-        Mono<Object> objectMono = exchange.getSession().flatMap(session -> {
-            String s = (String) session.getAttributes().get("1111111");
-            System.out.println("-----------------------" + s);
-
-            return null;
-        });
+    public BaseOut login(@RequestBody Map<String, Object> param, HttpServletRequest request) {
         String userCode = (String) param.get("userCode");
         log.info("[" + userCode + "]开始登录");
         String password = (String) param.get("password");
@@ -66,8 +59,8 @@ public class CommonController {
             }
             if (password.equals(eventUsersList.get(0).getPassword())) {
                 LOGGER.info("登录成功");
-              /*  HttpSession session = request.getSession();
-                session.setAttribute("eventUser", eventUsersList.get(0));*/
+                HttpSession session = request.getSession();
+                session.setAttribute("eventUser", eventUsersList.get(0));
                 return new BaseOut(ResultCode.SUCCESS);
             }else {
                 return new BaseOut(ResultCode.FAIL, "登录失败，密码错误");
